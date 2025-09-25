47cbdb59b952b5b79525737c8b0ce7c86919cedc
Stephen Popovich <stephenpopovich16@gmail.com>
2025-09-24 20:43:28 -0700

    Add MovieCard component with basic props

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,13 @@
-import './App.css'
+import './App.css';
+import MovieCard from './components/MovieCard'; 
 
 function App() {
   return (
     <>
-    <Text display= "Whats up?"/>
-    <Text display="Hello? Are you there?"/>
+      <MovieCard movie={{title: "Stephen's Film", release_date: "2024"}} />
+      <MovieCard movie={{title: "Alice's Film", release_date: "2026"}} />
     </>
-  )
-}
-
-function Text({display}){
-  return (
-   <div>
-    <p>{display}</p>
-   </div>
-  )
+  );
 }
 
 export default App
