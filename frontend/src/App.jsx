0f4edca7f117e4bf0e602121358efadc13ce5686
Stephen Popovich <stephenpopovich16@gmail.com>
2025-09-26 14:10:46 -0700

    Add conditional render to MovieCard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,16 @@ import './App.css';
 import MovieCard from './components/MovieCard'; 
 
 function App() {
+
+const movieNumber = 1;
+
   return (
     <>
+    {movieNumber === 1 ? (
       <MovieCard movie={{title: "Stephen's Film", release_date: "2024"}} />
+    ) : (
       <MovieCard movie={{title: "Alice's Film", release_date: "2026"}} />
+     )}
     </>
   );
 }
