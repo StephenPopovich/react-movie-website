90933b53bdabd270b749b71165400f5677ada11e
Stephen Popovich <stephenpopovich16@gmail.com>
2025-09-26 15:25:04 -0700

    Add pages Home and Favorites

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,11 @@
 import './App.css';
-import MovieCard from './components/MovieCard'; 
+import Home from './pages/Home';
 
 function App() {
 
-const movieNumber = 1;
-
   return (
     <>
-    {movieNumber === 1 ? (
-      <MovieCard movie={{title: "Stephen's Film", release_date: "2024"}} />
-    ) : (
-      <MovieCard movie={{title: "Alice's Film", release_date: "2026"}} />
-     )}
+    <Home />
     </>
   );
 }
