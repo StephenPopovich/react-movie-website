59b5597d86a42d9bcd762e448a4f69fc1fd9cd4c
Stephen Popovich <stephenpopovich16@gmail.com>
2025-09-26 18:09:29 -0700

    Import CSS files for components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
-import './App.css';
+import './css/App.css';
 import Home from './pages/Home';
 import Favorites from './pages/Favorites';
+import NavBar from './components/NavBar';
 import {Routes, Route} from 'react-router-dom';
 
 function App() {
 
   return (
+    <div>
+      <NavBar />
     <main className="main-content">
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/favorites" element={<Favorites />} />
     </Routes> 
     </main>
+  </div>
   );
 }
 
