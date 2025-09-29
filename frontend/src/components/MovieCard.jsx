fb415f2a61aef68ec7531877b809d0288195f6d9
Stephen Popovich <stephenpopovich16@gmail.com>
2025-09-29 16:49:23 -0700

    Update release dates for movies to be complete

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -16,7 +16,15 @@ function MovieCard({movie}){
     </div>
     <div className="movie-info"></div>
     <h3>{movie.title}</h3>
-    <p>{movie.release_date}</p>
+    <p>
+    {movie.release_date
+        ? new Date(movie.release_date).toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+        })
+        : ""}
+    </p>
  </div>
 }
 
